3a9e3064fd323b614321046fc297ba17f053fdf5
Vinod Kurup <vinod@kurup.com>
2013-01-06 14:36:48 -0500

    Initial commit

---
+++ tcx.py
@@ -0,0 +1,34 @@
+from lxml import objectify, etree
+
+
+class TcxParser:
+
+    def __init__(self, tcx_file):
+        tree = objectify.parse(tcx_file)
+        self.root = tree.getroot()
+        self.activity = self.root.Activities.Activity
+
+    def latitude(self):
+        return self.activity.Lap.Track.Trackpoint.Position.LatitudeDegrees
+
+    def longitude(self):
+        return self.activity.Lap.Track.Trackpoint.Position.LongitudeDegrees
+
+    def activity_type(self):
+        return self.activity.attrib['Sport'].lower()
+
+    def completed_at(self):
+        return self.activity.Lap[-1].Track.Trackpoint[-1].Time
+
+    def distance(self):
+        return self.activity.Lap[-1].Track.Trackpoint[-2].DistanceMeters
+
+    def distance_units(self):
+        return 'meters'
+
+    def duration(self):
+        """Returns duration of workout in seconds."""
+        return sum(lap.TotalTimeSeconds for lap in self.activity.Lap)
+
+    def calories(self):
+        return sum(lap.Calories for lap in self.activity.Lap)
---
+++ test_tcx.py
@@ -0,0 +1,36 @@
+import unittest
+from tcx import TcxParser
+
+
+class TestParseTcx(unittest.TestCase):
+
+    def setUp(self):
+        tcx_file = 'test.tcx'
+        self.tcx = TcxParser(tcx_file)
+
+    def test_latitude_is_correct(self):
+        self.assertEquals(self.tcx.latitude(), 35.951880198)
+
+    def test_longitude_is_correct(self):
+        self.assertEquals(self.tcx.longitude(), -79.0931872185)
+
+    def test_activity_type_is_correct(self):
+        self.assertEquals(self.tcx.activity_type(), 'running')
+
+    def test_completion_time_is_correct(self):
+        self.assertEquals(self.tcx.completed_at(), '2012-12-26T22:03:05Z')
+
+    def test_distance_is_correct(self):
+        self.assertEquals(self.tcx.distance(), 4686.31103516)
+
+    def test_distance_units_is_correct(self):
+        self.assertEquals(self.tcx.distance_units(), 'meters')
+
+    def test_duration_is_correct(self):
+        self.assertEquals(self.tcx.duration(), 1992.78)
+
+    def test_calories_is_correct(self):
+        self.assertEquals(self.tcx.calories(), 379)
+
+if __name__ == '__main__':
+    unittest.main()
