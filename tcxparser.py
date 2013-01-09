7829b467840786c01c38822183e6321ee1e2fc0c
Vinod Kurup <vinod@kurup.com>
2013-01-08 21:15:29 -0500

    Make methods into properties

--- tcxparser.py
+++ tcxparser.py
@@ -2,7 +2,7 @@
 
 from lxml import objectify
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 
 class TcxParser:
@@ -12,27 +12,35 @@ class TcxParser:
         self.root = tree.getroot()
         self.activity = self.root.Activities.Activity
 
+    @property
     def latitude(self):
         return self.activity.Lap.Track.Trackpoint.Position.LatitudeDegrees
 
+    @property
     def longitude(self):
         return self.activity.Lap.Track.Trackpoint.Position.LongitudeDegrees
 
+    @property
     def activity_type(self):
         return self.activity.attrib['Sport'].lower()
 
+    @property
     def completed_at(self):
         return self.activity.Lap[-1].Track.Trackpoint[-1].Time
 
+    @property
     def distance(self):
         return self.activity.Lap[-1].Track.Trackpoint[-2].DistanceMeters
 
+    @property
     def distance_units(self):
         return 'meters'
 
+    @property
     def duration(self):
         """Returns duration of workout in seconds."""
         return sum(lap.TotalTimeSeconds for lap in self.activity.Lap)
 
+    @property
     def calories(self):
         return sum(lap.Calories for lap in self.activity.Lap)
--- test_tcxparser.py
+++ test_tcxparser.py
@@ -9,28 +9,28 @@ class TestParseTcx(unittest.TestCase):
         self.tcx = TcxParser(tcx_file)
 
     def test_latitude_is_correct(self):
-        self.assertEquals(self.tcx.latitude(), 35.951880198)
+        self.assertEquals(self.tcx.latitude, 35.951880198)
 
     def test_longitude_is_correct(self):
-        self.assertEquals(self.tcx.longitude(), -79.0931872185)
+        self.assertEquals(self.tcx.longitude, -79.0931872185)
 
     def test_activity_type_is_correct(self):
-        self.assertEquals(self.tcx.activity_type(), 'running')
+        self.assertEquals(self.tcx.activity_type, 'running')
 
     def test_completion_time_is_correct(self):
-        self.assertEquals(self.tcx.completed_at(), '2012-12-26T22:03:05Z')
+        self.assertEquals(self.tcx.completed_at, '2012-12-26T22:03:05Z')
 
     def test_distance_is_correct(self):
-        self.assertEquals(self.tcx.distance(), 4686.31103516)
+        self.assertEquals(self.tcx.distance, 4686.31103516)
 
     def test_distance_units_is_correct(self):
-        self.assertEquals(self.tcx.distance_units(), 'meters')
+        self.assertEquals(self.tcx.distance_units, 'meters')
 
     def test_duration_is_correct(self):
-        self.assertEquals(self.tcx.duration(), 1992.78)
+        self.assertEquals(self.tcx.duration, 1992.78)
 
     def test_calories_is_correct(self):
-        self.assertEquals(self.tcx.calories(), 379)
+        self.assertEquals(self.tcx.calories, 379)
 
 if __name__ == '__main__':
     unittest.main()
