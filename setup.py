052cac696f044119efc34424b0e1778c77b90399
Vinod Kurup <vinod@kurup.com>
2013-01-13 10:15:21 -0500

    Add test suite to the distribution

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version=__version__,
     author='Vinod Kurup',
     author_email='vinod@kurup.com',
-    py_modules=['tcxparser', ],
+    py_modules=['tcxparser', 'test_tcxparser'],
     url='https://github.com/vkurup/python-tcxparser/',
     license='BSD',
     description='Simple parser for Garmin TCX files',
@@ -22,4 +22,5 @@ setup(
     install_requires=[
         "lxml",
     ],
+    test_suite="test_tcxparser",
 )
