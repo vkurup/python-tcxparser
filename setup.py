af98df7a4c6baca9ec6923110bc89e9ee81e3218
Vinod Kurup <vinod@kurup.com>
2018-12-15 16:00:46 -0500

    Try 3.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     install_requires=[
